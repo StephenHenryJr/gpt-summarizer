ba4bd5a2fd2c0dbfeb4473fa330703eb98085b7d
StephenHenryJr <shenryjr_85@hotmail.com>
2023-05-29 08:39:36 -0400

    Header section completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,33 @@
+import { logo } from "../assets";
+
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
 
-export default Hero
+        <button
+          type="button"
+          onClick={() =>
+            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+          }
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Discover the magic of <br className="max-md:hidden" />
+        <span className="orange_gradient ">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Streamline your reading experience with Summarize, a free and
+        open-source article summarizer that condenses extensive articles into
+        succinct and easily digestible summaries.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
