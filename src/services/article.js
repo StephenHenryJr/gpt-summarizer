b691f8e543995d7c16c24f7fe7c0a046b555ae20
StephenHenryJr <shenryjr_85@hotmail.com>
2023-06-03 17:22:27 -0400

    Added history section and added copy functionality

--- src/services/article.js
+++ src/services/article.js
@@ -1,14 +1,14 @@
 // These functions are part of the Redux Toolkit Query package, which is used for making API requests and managing the data returned from those requests.
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
 
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', rapidApiKey);
+            headers.set('X-RapidAPI-Key', 'dc0c32a409msh847f1dac53b04a2p14928cjsn1c430baa58a3');
             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
 
             return headers;
